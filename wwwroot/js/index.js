047b798ffa05f0ecdd326a4c2baf2a844c008879
Stephen Odundo <steveremdi@gmail.com>
2020-02-17 21:44:31 +0300

    Added javascript, jquery, bootstrap and npm

---
+++ wwwroot/js/index.js
@@ -0,0 +1,16 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hello Pluralsight");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function buyFunction() {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+
